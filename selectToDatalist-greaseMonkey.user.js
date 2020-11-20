5ea234dc52e1c7d9bcd89574d0375807fe2b7210
Andrew Nordlund <andrew.nordlund@hrsdc-rhdcc.gc.ca>
2020-11-20 13:05:43 -0500

    I think the GM script is prod-ready.

--- selectToDatalist-greaseMonkey.user.js
+++ selectToDatalist-greaseMonkey.user.js
@@ -4,93 +4,141 @@
 // @grant    none
 // @description Convert a select to a datalist
 // @include     */selectToDatalist/*
+// @include	    *cgi-bin/scdtic-ictdcs/add-page-error.aspx*
 // @version     1
 // @run-at      document-end
 // ==/UserScript==
 
-console.log ("convert::BLAHLABLABLA");
+if (dbug) console.log ("convert::running");
 var selects = null;
 var datalists = [];
-var dbug = true;
+var dbug = false;
+
 selects = document.getElementsByTagName("select");
 if (selects) {
-  console.log ("Got " + selects.length + " selects");
+  console.log ("convert::Got " + selects.length + " selects");
   
-  for (var i = selects.length-1; i >-1; i--) {
-		if (dbug) console.log ("Doing select: " + i);
-		var parentNode = selects[i].parentNode;
-		var newInput = document.createElement("input");
+  for (let i = selects.length-1; i >-1; i--) {
+    let opts = null;
+    opts = selects[i].querySelectorAll("option");
+    if (opts.length > 20 || selects[i].getAttribute("id") == "MainContent_Errors") {
+			if (dbug) console.log ("convert::Doing select: " + i + " with " + opts.length + " options");
+      
+      // First get the select's parentnode, ID, and labelID
+			var parentNode = selects[i].parentNode;
+      var selID, label, labelID = null;
+      if (selects[i].hasAttribute("id")) {
+        selID = selects[i].getAttribute("id");
+      } else {
+        selID = "selects-" + i;
+        if (document.getElementById(selID)) {
+          // Find a new ID
+          selID = "selects-blahblah-" + i;	// Imma gamble this ID won't exist on the page
+        }
+        selelects.setAttribute("id", selID);
+      }
+      
+      if (dbug) console.log ("convert::Got sel.  Looking for label");
+      label = parentNode.querySelector("label[for='" + selID + "']");
+      if (label) {
+        if (label.hasAttribute("id")) {
+          labelID = label.getAttribute("id");
+      	} else {
+        	labelID = "sel-a" + i + "-Lbl";
+        	label.setAttribute("id", labelID);
+      	}
+      } else {
+        if (label.hasAttribute("aria-labelledby")) {
+          labelID = label.getAttribute("aria-labelledby");
+        }
+      }
+      
+      if (dbug) console.log ("convert::Got label.  Now creating new input");
+      // Create new input
+			var newInput = document.createElement("input");
+    	if (labelID) newInput.setAttribute("aria-labelledby", labelID);
+      newInput.setAttribute("form", "jafafkjadfjkafd");
+      newInput.setAttribute("aria-controls", selID);
+           
+      //newInput.setAttribute("width", 100);
+      newInput.setAttribute("style", "display:block;min-width: 50em; max-width: 95%;");
+      
+      parentNode.insertBefore(newInput, selects[i]);
+      
+      // Create the datalist to mimic the select
+			var datalist = document.createElement("datalist");
+			datalist.setAttribute("id", "datalist" + (i+1));
 		
-		newInput.setAttribute("name", selects[i].getAttribute("name"));
-		newInput.setAttribute("id", selects[i].getAttribute("id"));
+    	// Populate it with options
+			var hasValues = false;
+			for (var j = 0; j< opts.length; j++) {
+				var newOpt = document.createElement("option");
+				newOpt.innerHTML = opts[j].innerHTML;
+				if (opts[j].hasAttribute("value")) {
+					newOpt.setAttribute("value", opts[j].getAttribute("value"));
+					hasValues = true;
+				}
+				datalist.appendChild(newOpt);
+      }
+      parentNode.appendChild(datalist);
+      
+      newInput.setAttribute("list", "datalist" + (i+1));
+      newInput.addEventListener("change", setSelVal, false);
+    		
 		
-		var opts = selects[i].querySelectorAll("option");
-		var datalist = document.createElement("datalist");
-		datalist.setAttribute("id", "datalist" + (i+1));
-		
-		var hasValues = false;
-		for (var j = 0; j< opts.length; j++) {
-			var newOpt = document.createElement("option");
-			newOpt.innerHTML = opts[j].innerHTML;
-			if (opts[j].hasAttribute("value")) {
-				newOpt.setAttribute("value", opts[j].getAttribute("value"));
-				hasValues = true;
-			}
-			datalist.appendChild(newOpt);
-		}
-		if (hasValues) {
-			var newValInput = document.createElement("input");
-			newValInput.setAttribute("name",  selects[i].getAttribute("name"));
-			newValInput.setAttribute("id", selects[i].getAttribute("name") + i);
-			newValInput.setAttribute("style", "position: absolute; left: -1000px");
-			newInput.setAttribute("name", selects[i].getAttribute("name") + i);
-			newInput.addEventListener("change", setVal, false);
-			newInput.setAttribute("form", "akdskjajfkd");
-		}
-
-		
-		newInput.setAttribute("list", "datalist" + (i+1));
-		
-		parentNode.appendChild(newInput);
-		if (hasValues) parentNode.appendChild(newValInput);
-		parentNode.appendChild(datalist);
-		parentNode.removeChild(selects[i]);
+    }
 	}
 }
 
+function setSelVal (e) {
+  
+  var el = e.target;
+  var optVal = el.value;
+  let opts = null;
+	if (dbug) console.log ("convert::Set value: " + optVal + ".");
 
-function setVal(e) {
-	var el = e.target;
-
-	var optVal = el.value;
-	console.log ("Set value: " + optVal + ".");
-	var num, dlID, dl, name = null;
-	name = el.getAttribute("name");
-	dlID = el.getAttribute("list");
-	if (dbug) console.log ("Looking for list with ID: " + dlID + ".");
-	dl = document.getElementById(dlID);
-	num = el.getAttribute("id").match(/\d+$/);
-	if (num) {
-		num = num.join();
-		var opt, newValInput = null;
-		opt = dl.querySelector("option[value='" + optVal + "']");
-		if (opt) {
-			optName = opt.innerHTML;
-			newValInput = document.getElementById(name);
-			if (newValInput) {
-				newValInput.value = optVal;
-				el.value = opt.innerHTML;
-				newValInput.setAttribute("type", "hidden");
-			} else {
-				if (dbug) console.log ("Couldn't find newValInput: " + name + ".");
-			}
-		} else {
-			if (dbug) console.log ("Didn't find option.");
-		}
-
-
-
-	}
-
-}
-
+  var selID, selectEl=null;
+  selID = el.getAttribute("aria-controls");
+  if (dbug) console.log ("selID: " + selID);
+  selectEl = document.getElementById(selID);
+  if (dbug) console.log ("selectEL: " + selectEl.getAttribute("id"));
+  try {
+		opts = selectEl.querySelectorAll("option");
+  }
+  catch (ex) {
+    console.log ("Convert::setSelVal::Error: " + ex.message);
+  }
+  if (dbug) console.log ("convert::setSelVal::opts: " + opts + ".");
+  
+  var looking = true;
+  if (dbug) console.log ("convert::setSelVal::Before I get looking there are " + opts.length + " options to look through.");
+  for (let i = 0; i < opts.length && looking; i++) {
+    if (opts[i].hasAttribute("value")) {
+      if (dbug) console.log ("opts[" + i + "] has value (" + opts[i].value + ") and comparing to " + optVal + ".");
+      
+      if (opts[i].getAttribute("value") == optVal) 
+      {
+        if (dbug) console.log ("Found it by value!");
+      
+        selectEl.selectedIndex=i;
+        looking = false;
+        el.value = opts[i].text;
+      }
+      
+  	} 
+    else 
+    {
+      if (dbug) console.log ("opts[" + i + "] has text (" + opts[i].text + ")");
+    	if (opts[i].text == optVal) 
+      {
+	      if (dbug) console.log ("Found it by text!");
+  	    selectEl.selectedIndex = i;
+    	  looking = false;
+      	el.value = opts[i].text;
+    	}
+      
+    } // End if if.opts.hasValue
+    if (dbug) console.log (i +": Continuing on to the next opt with looking as " + looking + " opts.length = " + opts.length + ".");
+  } // End of for.opts.length loop
+  
+} // End of setSelVal
